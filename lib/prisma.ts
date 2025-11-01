811c431d416e2620f4fdec0a28e0a12f9589625e
Abhishek Pandey <abhishekpandey1160@gmail.com>
2025-11-01 17:23:57 +0530

    Fix Prisma client generation path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,11 @@
-import { PrismaClient } from './generated/prisma';
+import { PrismaClient } from '@prisma/client';
 import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient().$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
